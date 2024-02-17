84dd5f0dfa0cfe44961cb4b68afd7c2a2792556a
Grizzly21f <taras.firhuk72@gmail.com>
2024-02-17 14:48:22 +0200

    Weather

--- src/middlewares/weather.middleware.ts
+++ src/middlewares/weather.middleware.ts
@@ -1,20 +1,33 @@
 import { NextFunction, Request, Response } from "express";
 
 import { ApiError } from "../errors/api.error";
+import { IQuery } from "../types/params.type";
 
 class ParamsMiddleware {
   public async checkParams(req: Request, res: Response, next: NextFunction) {
     try {
-      const param = req.query;
+      const param = req.query as IQuery;
 
-      if (!param) {
-        throw new ApiError("wrong", 400);
+      if (param.city && param.lat && param.lon) {
+        throw new ApiError("Wrong", 406);
+      }
+      if (Object.keys(req.query).length === 0) {
+        throw new ApiError("Bad Request", 400);
+      }
+      if (param.city && (param.lat || param.lon)) {
+        throw new ApiError("Bad Request", 400);
+      }
+      if ((param.lon && !param.lat) || (!param.lon && param.lat)) {
+        throw new ApiError("Bad Request", 400);
       }
 
       res.locals = param;
       next();
     } catch (e) {
-      next(e);
+      if (e instanceof ApiError) {
+        return res.status(e.status).json({ error: e.message });
+      }
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
 }
